429048efb9b03ac7d64e8d52145d96639e03e044
Sawaira-Iftikhar <isawairaiftikhar@gmail.com>
2026-09-06 01:22:03 +0500

    did some changings in details

--- 02_Data_and_operators.py
+++ 02_Data_and_operators.py
@@ -2,7 +2,6 @@
 ============================================
   LECTURE 1 - FILE 2: DATA & OPERATORS
   Topics: Data Types, Types of Operators
-  Difficulty: 🟡 Medium
   Total Questions: 
 ============================================
 """
