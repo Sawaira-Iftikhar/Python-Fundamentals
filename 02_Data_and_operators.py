5817146198eeb47843061a6a944e5f2d6fcad8b1
Sawaira-Iftikhar <isawairaiftikhar@gmail.com>
2026-09-06 01:20:28 +0500

    about file

---
+++ 02_Data_and_operators.py
@@ -0,0 +1,8 @@
+"""
+============================================
+  LECTURE 1 - FILE 2: DATA & OPERATORS
+  Topics: Data Types, Types of Operators
+  Difficulty: 🟡 Medium
+  Total Questions: 
+============================================
+"""
