3d60546b0385cf228514ea0f118255a519e8f710
Sawaira-Iftikhar <isawairaiftikhar@gmail.com>
2026-09-06 23:56:56 +0500

    and total question nubmer on the start of th file

--- 02_Data_and_operators.py
+++ 02_Data_and_operators.py
@@ -2,7 +2,7 @@
 ============================================
   LECTURE 1 - FILE 2: DATA & OPERATORS
   Topics: Data Types, Types of Operators
-  Total Questions: 
+  Total Questions: 9
 ============================================
 """
 
