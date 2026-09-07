53125a48f9ff500eaa2776767c0b6fddbb8f12c2
Sawaira-Iftikhar <isawairaiftikhar@gmail.com>
2026-09-07 17:30:53 +0500

    add details form changing file decsrption

---
+++ 04_challenge.py
@@ -0,0 +1,10 @@
+"""
+============================================
+  LECTURE 1 - FILE 4: BOSS CHALLENGE 🔥
+  Topics: ALL 6 Topics Combined
+  Total Challenges: 3
+============================================
+  These problems test EVERYTHING you learned
+  in Lecture 1.
+============================================
+"""
