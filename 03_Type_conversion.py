76c52a9eefc5a02a2d943d4f5b3557a685a6857b
Sawaira-Iftikhar <isawairaiftikhar@gmail.com>
2026-09-07 05:22:30 +0500

    add Q1 in part A

--- 03_Type_conversion.py
+++ 03_Type_conversion.py
@@ -10,3 +10,10 @@
 #  PART A: EXPLICIT CONVERSION / Type Casting
 # ============================================
 
+# Q1. Convert the following and print each result WITH its type:
+#     a) float 9.99  → int
+#     b) string "42" → int
+#     c) int 100     → float
+#     d) int 1       → bool
+#     e) int 0       → bool
+#     f) int 7       → string
