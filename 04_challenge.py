4b900e701371119358807b27e9b20c235528c606
Sawaira-Iftikhar <isawairaiftikhar@gmail.com>
2026-09-07 18:42:26 +0500

    add on some setting up the details

--- 04_challenge.py
+++ 04_challenge.py
@@ -2,7 +2,7 @@
 ============================================
   LECTURE 1 - FILE 4: BOSS CHALLENGE 
   Topics: ALL 6 Topics Combined
-  Total Challenges: 3
+  Total Challenges: 
 ============================================
   These problems test EVERYTHING you learned
   in Lecture 1.
